7157e930179fc9859a889d78ca51c2dc152664e4
ltadeu6 <ltadeu6@protonmail.com>
2021-01-28 17:55:38 -0300

    config repo alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 export EDITOR=nvim
 export PATH="/home/ltadeu6/.gem/ruby/2.7.0/bin:$PATH"
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 #alias vi="nvim"
 #alias vim="nvim"
 #alias ls="lsd"
